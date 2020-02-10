1c7e2a64f4dac39e9b9697497613ddb6aad9ad55
flan <flan@flande.re>
2020-02-10 19:04:15 +0100

    alterdec: use fixed width types
    `unsigned long` is not guaranteed to always be 32-bit long.
    On many 64-bit platforms it's actually 64-bit long, which
    makes CStream::read* behave unexpectedly:
    
    	dword CStream::readdword()
    	{
    		dword d; // not zeroed, possibly filled with garbage
    		read( &d, 4 ); // only the first 4 bytes are changed
    		return d; // 4 bytes + garbage are returned
    	}
    
    This triggers a bug in common.cpp:/\*ReadCrypted/:
    
    	dword size1 = ~( s->readdword() ^ 0xc92e568b );
    	dword size2 = ( s->readdword() ^ 0xc92e568f ) / 8;
    
    CStream::read* assume `dword`s (aliased to `unsigned long`) are
    no longer than 32 bits. In case they actually are longer, the next
    line may trigger a very large allocation (close to 16TiB in some
    cases):
    
    	byte *buf = (byte *)malloc( size2 );
    
    Note that the current code still doesn't account for endianness.
    CStream::read(void *, int) reads from the file directly into the
    output variable (`dword d` in case of CStream::readdword). If the
    platform is big-endian, the byte order of the output variable may
    be incorrect.

--- Rugp/alterdec/src2/core.h
+++ Rugp/alterdec/src2/core.h
@@ -3,7 +3,8 @@
 
 
 
-typedef unsigned char		byte;
-typedef unsigned short		word;
-typedef unsigned long		dword;
-typedef unsigned long long	qword;
+#include <cstdint>
+typedef uint8_t  byte;
+typedef uint16_t word;
+typedef uint32_t dword;
+typedef uint64_t qword;
